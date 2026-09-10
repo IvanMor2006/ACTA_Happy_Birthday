454c3c5a3933f3f18469cd32487ae15dcde1a1ea
IvanMor2006 <118119868+IvanMor2006@users.noreply.github.com>
2026-09-10 22:15:34 +0300

    права на подарки

--- app/sp_gift.py
+++ app/sp_gift.py
@@ -4,18 +4,8 @@ from .decorators import *
 
 sp_gift_bp = Blueprint('sp_gift', __name__, url_prefix='')
 
-@sp_gift_bp.route('sp/pay')
-@role_required(['admin', 'admin_viewer'])
-def sp():
-    db = get_db()
-    pays = db.execute("select * from v_pay_info pay where pay != price order by t_d desc, t_t desc").fetchall()
-    pays_done = db.execute("select * from v_pay_info pay where pay = price order by t_d desc, t_t desc").fetchall()
-    people_ol = db.execute('select ku, naim from sp_ol where in_theater and could_pay order by naim').fetchall()
-    people_dr = db.execute("select ku, naim, cy_dr from v_dr_info order by to_date(cy_dr, 'dd.mm.yyyy') desc, naim").fetchall()
-    return render_template('sp/pay/sp.html', pays=pays, pays_done=pays_done, people_ol=people_ol, people_dr=people_dr)
-
 @sp_gift_bp.route('/sp/ol/<int:ku_ol>/gift/add', methods=['POST'])
-@role_required(['admin'])
+@login_required
 def add(ku_ol):
     ref = request.referrer
 
@@ -43,7 +33,7 @@ def add(ku_ol):
     return redirect(ref)
 
 @sp_gift_bp.route('/sp/ol/<int:ku_ol>/gift/delete', methods=['POST'])
-@role_required(['admin'])
+@login_required
 def delete(ku_ol):
     ref = request.referrer
     ku = request.form.get('d_gift_ku')
